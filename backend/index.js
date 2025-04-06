840e402f06d3199631e67e33343b79cb5649dc61
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-04-07 02:00:24 +0530

    MongoDB-connection-added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-
-
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv"
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 app.listen(port,()=>{
+    connectDB();
     console.log(`Server is running in http://localhost:${port}`);
 })
