6ebad6374d067a4da28c2e487f08ffb38d5b80fd
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-04-21 19:23:58 +0530

    login-added

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -37,7 +37,20 @@ export const useAuthStore = create((set)=> ({
             throw error;
         }        
     },
-
+    login : async (email, password) => {
+        set({ isLoading: true, error: null});
+        try {
+            const response = await axios.post(`${API_URL}/login`,{email, password});
+            set({
+                isAuthenticated: true,
+                user: response.data.user,
+                error: null,
+                isLoading: false
+            });
+        } catch (error) {
+            set({ error: error.response?.data?.messsage || "Error logging in", isLoading: false});
+        }
+    },
     checkAuth: async () => {
         try {
             const response = await axios.get(`${API_URL}/check-auth`);
