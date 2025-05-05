6e63217a90a8889227b2af93ecb6579fe7480a01
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-05-05 23:40:16 +0530

    cookie-bug-fixed in production

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (res, userId)=>{
     res.cookie("token", token, {
         httpOnly: true,//cookie can not be accessable from client side JS
         secure: process.env.NODE_ENV === "production", //if it is in development it will be HTTP else if it will be HTTPS
-        sameSite: "strict", //prevents csrf attack at backend
+        sameSite: "None", //prevents csrf attack at backend
         maxAge: 7*24*60*60*1000,
     });
 
